9e1b1937a624d03ca6c07aa625b942a8996c05db
Patric Pereira <patric.pereirafranca@gmail.com>
2019-02-09 11:46:14 -0200

    trabalhando com props

--- index.android.js
+++ index.android.js
@@ -1,53 +1,24 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
 
-export default class app_3 extends Component {
+class MeuComponente extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+      <View>
+        <Text>{this.props.propriedade1}</Text>
+        <Text>{this.props.xyz}</Text>
+        <Text>{this.props.p}</Text>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+class app_3 extends Component {
+  render() {
+    return (
+      <MeuComponente propriedade1='Banana' xyz='Abacaxi' p='Uva'></MeuComponente>
+    );
+  }
+}
 
 AppRegistry.registerComponent('app_3', () => app_3);
