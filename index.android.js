19d37f3cf38b707d9cc3c512c64b69fb7608789a
Patric Pereira <patric.pereirafranca@gmail.com>
2019-02-09 12:51:21 -0200

    logica do jogo - parte III

--- index.android.js
+++ index.android.js
@@ -6,7 +6,11 @@ class app_3 extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { escolhaUsuario: '', escolhaComputador: '' };
+    this.state = { 
+      escolhaUsuario: '', 
+      escolhaComputador: '',
+      resultado: ''
+     };
   }
 
   jokenpo(escolhaUsuario) {
@@ -20,9 +24,55 @@ class app_3 extends Component {
       case 2: escolhaComputador = 'tesoura'; break;
     }
 
-    this.setState({ escolhaUsuario: escolhaUsuario, 
-                    escolhaComputador: escolhaComputador 
-                  });
+    let resultado = '';
+
+    if (escolhaComputador === 'pedra') {
+      if (escolhaUsuario === 'pedra') {
+        resultado = 'Empate';
+      }
+
+      if (escolhaUsuario === 'papel') {
+        resultado = 'Usuário ganhou';
+      }
+
+      if (escolhaUsuario === 'tesoura') {
+        resultado = 'Computador ganhou';
+      }
+    }
+
+    if (escolhaComputador === 'papel') {
+      if (escolhaUsuario === 'papel') {
+        resultado = 'Empate';
+      }
+
+      if (escolhaUsuario === 'tesoura') {
+        resultado = 'Usuário ganhou';
+      }
+
+      if (escolhaUsuario === 'pedra') {
+        resultado = 'Computador ganhou';
+      }
+    }
+
+    if (escolhaComputador === 'tesoura') {
+      if (escolhaUsuario === 'tesoura') {
+        resultado = 'Empate';
+      }
+
+      if (escolhaUsuario === 'pedra') {
+        resultado = 'Usuário ganhou';
+      }
+      
+      if (escolhaUsuario === 'papel') {
+        resultado = 'Computador ganhou';
+      }
+    }
+
+    this.setState({ 
+      escolhaUsuario: escolhaUsuario, 
+      escolhaComputador: escolhaComputador,
+      resultado: resultado
+    });
   }
 
   render() {
@@ -30,7 +80,7 @@ class app_3 extends Component {
       <View>
         <Text>Escolha do computador: {this.state.escolhaComputador}</Text>
         <Text>Escolha do usuário: {this.state.escolhaUsuario}</Text>
-        <Text>Resultado</Text>
+        <Text>Resultado: {this.state.resultado}</Text>
         <Button title="pedra" onPress={() => { this.jokenpo('pedra'); }} />
         <Button title="papel" onPress={() => { this.jokenpo('papel'); }} />
         <Button title="tesoura" onPress={() => { this.jokenpo('tesoura'); }} />
